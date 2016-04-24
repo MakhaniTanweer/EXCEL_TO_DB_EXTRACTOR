6a37d9ba19b9117d5c5f57f12139c8af2c86f5d4
Tanweer <Tanweer>
2016-04-24 13:25:43 +0500

    DONE

--- main.php
+++ main.php
@@ -8,11 +8,15 @@ function checkTypes($values,$schema,$mapping)
 		$type = $value['Type'];
 		//var_dump($values[0][intval($mapping[$key])]);
 		//var_dump($type);
-		if(strpos(strtolower($type),'varchar'))
+
+		if( intval($mapping[$key])=== 10000 || intval($mapping[$key]) === -1 )
+			continue;
+
+		if(strpos(strtolower($type),'varchar') !== false)
 		    if(!is_string($values[0][$mapping[$key]]))
 				$flag = false;
 
-		if(strpos(strtolower($type),'int') || strpos(strtolower($type),'numeric'))
+		if(strpos(strtolower($type),'int') !== false || strpos(strtolower($type),'numeric') !== false)
 		    if(!is_float($values[0][$mapping[$key]]))
 				$flag = false;
 
@@ -32,18 +36,42 @@ function addToDatabase($excelData,$schema,$mapping,$connection,$DestTable)
 	}
 
 	$quer = "INSERT INTO ".$DestTable." VALUES (".$str.")";
-	echo $quer;
+	//echo $quer;
 	$stmt = $connection->prepare($quer);
-	var_dump($stmt);
-	$refs = array('dssss');
+	//var_dump($stmt);
+	$types = array();
+	foreach ($schema as $key => $value) {
+		$type = $value['Type'];
+		if(strpos(strtolower($type),'varchar') !== false)
+			$types[] = 's';
+		else if (strpos(strtolower($type),'int') !== false)
+			$types[] = 'i';
+		else if (strpos(strtolower($type),'numeric') !== false)
+			$types[] = 'i';
+		else if (strpos(strtolower($type),'decimal') !== false)
+			$types[] = 'd';
+		else if (strpos(strtolower($type),'date') !== false)
+			$types[] = 's';
+		else
+			$types[] = 's';
+	}
+	$datatype = implode($types);
+	$refs = array($datatype);
+
 	foreach ($excelData[0] as $key => $value)
 	{
-	  var_dump($mapping[$key]);
-	  $refs[] =& $excelData[0][intval($mapping[$key])];
+	  if( intval($mapping[$key])=== 10000 || intval($mapping[$key]) === -1 )
+	  {
+	  	$nul = null;
+	  	$refs[] = & $nul;
+	  }
+	  else
+	  	$refs[] =& $excelData[0][intval($mapping[$key])];
 	}
-	
+	//var_dump($refs);
 	call_user_func_array(array($stmt,'bind_param'),$refs);
-	var_dump($refs);
+
+	//var_dump($stmt);
 	$stmt->execute();
 };
 
@@ -53,6 +81,8 @@ $DestDB       = $_POST["DB"];
 $DestTable    = $_POST["Tab"];
 $mode         = $_POST["mode"];
 $map          = $_REQUEST["arr"];
+
+//var_dump($map);
 
 $Database = $DestDB;
 $tablename = $DestTable;
@@ -91,9 +121,14 @@ $highestColumn = $sheet->getHighestColumn();
 
 $flag = true;
 
+$succ = 0;
+$unsucc = 0;
+$errorlog = array();;
+
 //  Loop through each row of the worksheet in turn
-for ($row = 2; $row <= $highestRow && $flag; $row++){ 
+for ($row = 2; $row <= $highestRow; $row++){ 
     //  Read a row of data into an array
+    $unsucc++;
     $rowData = $sheet->rangeToArray('A' . $row . ':' . $highestColumn . $row,
                                     NULL,
                                     TRUE,
@@ -101,11 +136,21 @@ for ($row = 2; $row <= $highestRow && $flag; $row++){
 
     if(checkTypes($rowData,$feilds,$map))
     {
+    	$succ++;
+    	//echo "string";
     	addToDatabase($rowData,$feilds,$map,$sourceConnection,$DestTable);
     }   
     else
-    	$flag = false;
+    {
+    	$errorlog [] = implode(" ",$rowData[0]);
+    }
     //print_r($rowData);
 }
-
+$errorlist  = implode("\n",$errorlog);
+$result = array();
+$unsucc = $unsucc - $succ;
+$result[] = $succ;
+$result[] = $unsucc;
+$result[] = $errorlist;
+echo json_encode($result);
 ?>
